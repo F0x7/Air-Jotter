831a09cc896aa69b243928bfa2c6067a42eab6cc
F0x7 <Fox@macbook-pro.local>
2016-05-29 14:41:42 +0300

    Initial Commit

---
+++ Air Jetter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Air Jetter
+//
+//  Created by Fox Lis on 29.05.16.
+//  Copyright © 2016 Egor Bakaykin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
